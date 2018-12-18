ff1da69de0d943bb59952c99e67b75403ac7c527
alexg622 <alex.gm62288@gmail.com>
2018-12-17 21:57:11 -0800

    Style ratings

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -13,4 +13,45 @@ class Activity < ApplicationRecord
     self.ratings.each {|rating| average += rating.stars}
     return (average/self.ratings.length).round
   end
+
+  def get_comment
+    comment = nil
+    self.ratings.each do |rating|
+      if rating.stars == 5
+        comment = rating.comment.split(".")[0]
+      end
+    end
+    if comment == nil
+      self.ratings.each do |rating|
+        if rating.stars == 4
+          comment = rating.comment.split(".")[0]
+        end
+      end
+    end
+    if comment == nil
+      self.ratings.each do |rating|
+        if rating.stars == 3
+          comment = rating.comment.split(".")[0]
+        end
+      end
+    end
+    if comment == nil
+      self.ratings.each do |rating|
+        if rating.stars == 2
+          comment = rating.comment.split(".")[0]
+        end
+      end
+    end
+    if comment == nil
+      self.ratings.each do |rating|
+        if rating.stars == 1
+          comment = rating.comment.split(".")[0]
+        end
+      end
+    end
+    if comment == nil
+      comment =""
+    end
+    return comment
+  end
 end

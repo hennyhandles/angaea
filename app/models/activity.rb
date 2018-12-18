1704ac69685232aad345088b688dac3461550b17
alexg622 <alex.gm62288@gmail.com>
2018-12-17 21:30:40 -0800

    Make ratings card

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,16 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  has_many :ratings 
+  has_many :ratings
   #has many attributes
   validates :user_id, presence: true
   validates :content, presence: true, length: { minimum: 1 }
 
   # mount_uploader :picture, PictureUploader
   default_scope -> { order(created_at: :desc) }
+
+  def get_average_rating
+    average = 0
+    self.ratings.each {|rating| average += rating.stars}
+    return (average/self.ratings.length).round
+  end
 end

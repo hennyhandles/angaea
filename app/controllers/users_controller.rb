446e5f61dd5994e5a49342c367fa075969d271bb
Aditya Nachiappan <adityanachiappan@Adityas-MacBook-Pro.local>
2018-12-18 12:07:28 -0800

    usr controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,77 +1,73 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+ before_action :logged_in_user, only: [:index, :edit, :update]
+ before_action :correct_user, only: [:edit, :update]
 
 
-  def index
-    @users = User.all
-  end
+ def index
+   @users = User.all
+ end
 
 
-  def show
-    @user = User.find(params[:id])
-<<<<<<< HEAD
-    @activities = @user.activities 
-=======
-    @activity = Activity.new
-    @activities = @user.activities #need on bottom of page
->>>>>>> 99f2aabcb4debec9074c5853db53549d4e41d68b
-  end
+ def show
+   @user = User.find(params[:id])
+   @activity = Activity.new
+   @activities = @user.activities #need on bottom of page
+ end
 
-  def new
-    @user = User.new
-  end
+ def new
+   @user = User.new
+ end
 
-  def create
-      @user = User.new(user_params)
-      if @user.save
-        log_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user  #redirect_to user_url(@user)
-      else
-        render 'new'
-      end
-    end
-    #forgot to add log in user before that s why it didnt work
-
-   def edit
-       @user = User.find(params[:id])
-    end
-
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
-    end
-
-   private
-
-     def user_params
-       params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
+ def create
+     @user = User.new(user_params)
+     if @user.save
+       log_in @user
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user  #redirect_to user_url(@user)
+     else
+       render 'new'
      end
-         # Confirms a logged-in user.
-    def logged_in_user
-       unless logged_in?
-         store_location
-         flash[:danger] = "Please log in."
-         redirect_to login_url
-       end
-    end
+   end
+   #forgot to add log in user before that s why it didnt work
 
-    # Confirms the correct user.
-    def correct_user
+  def edit
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+   end
+
+
+ def update
+   @user = User.find(params[:id])
+   if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+     redirect_to @user
+   else
+     render 'edit'
+   end
+ end
+
+   def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) unless @user == current_user
+   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
+        # Confirms a logged-in user.
+   def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+   end
+
+   # Confirms the correct user.
+   def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+   end
 end

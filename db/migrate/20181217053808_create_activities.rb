c28059f93d6ab3309fdc17af3014a5ad70ecb97a
alexg622 <alex.gm62288@gmail.com>
2018-12-17 22:55:26 -0800

    Add to seed file

--- db/migrate/20181217053808_create_activities.rb
+++ db/migrate/20181217053808_create_activities.rb
@@ -2,12 +2,12 @@ class CreateActivities < ActiveRecord::Migration[5.2]
   def change
     create_table :activities do |t|
       #generate name
-    
+
       t.text :content
       t.string :additional_info
       t.references :user, foreign_key: true
       t.float :cost
-      t.string :type #dance music or art
+      t.string :category #dance music or art
       t.string :addressLN1
       t.string :addressLN2
       t.string :city

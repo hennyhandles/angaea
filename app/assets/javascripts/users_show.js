0b6c0db391b2af6b8a95816297d8ac05134c35b1
alexg622 <alex.gm62288@gmail.com>
2018-12-18 11:21:34 -0800

    Make toggle for create form in users show page

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -1,18 +1,43 @@
 $(document).on('turbolinks:load', () => {
   showRatings()
   showActivities()
+  showCreateActivityForm()
 })
 
 const showRatings = function() {
+  let ratings = document.querySelector(".users-ratings-container")
   let activities = document.querySelector(".users-activities-container")
+  let form = document.querySelector(".create-activity-form")
   document.querySelector(".right-toggle").addEventListener("click", (e) => {
+    console.log(ratings.style.display);
+    ratings.style.display = "flex"
     activities.style.display = "none"
+    form.style.display = "none"
   })
 }
 
 const showActivities = function() {
+  let ratings = document.querySelector(".users-ratings-container")
   let activities = document.querySelector(".users-activities-container")
+  let form = document.querySelector(".create-activity-form")
   document.querySelector(".left-toggle").addEventListener("click", (e) => {
-    activities.style.display = ""
+    activities.style.display = "flex"
+    form.style.display = "none"
+    ratings.style.display = "none"
   })
+}
+
+const showCreateActivityForm = function() {
+  let ratings = document.querySelector(".users-ratings-container")
+  let activities = document.querySelector(".users-activities-container")
+  let form = document.querySelector(".create-activity-form")
+  let toggleCreate = document.querySelector(".create-activity-toggle")
+  if(toggleCreate) {
+    toggleCreate.addEventListener("click", (e) => {
+      ratings.style.display = "none"
+      activities.style.display = "none"
+      form.style.display = "block"
+    })
+  }
+
 }
